0a04303fb7ca7b1192e123c446bf59d637ca1f2a
荷包蛋 <43177271+GG4EVER@users.noreply.github.com>
2019-11-19 16:56:20 +0800

    修改了全局滑块的背景颜色

--- src/App.vue
+++ src/App.vue
@@ -39,4 +39,14 @@
     .el-tabs--border-card>.el-tabs__content{
         padding: 0 10px 0 0 !important;
     }
+
+    /*滑动块禁止时的颜色(机器验证成功的颜色)*/
+    .el-slider__runway.disabled .el-slider__bar{
+        background-color: #67C23A !important;
+    }
+
+    /*滑动块禁止时按钮的颜色(机器验证成功的颜色)*/
+    .el-slider__runway.disabled .el-slider__button {
+        border-color: #67C23A !important;
+    }
 </style>

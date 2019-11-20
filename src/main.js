ab39c156923161b9bcc0b3c1c992f70d3184d429
荷包蛋 <43177271+GG4EVER@users.noreply.github.com>
2019-11-20 20:06:06 +0800

    解决两次访问相同路由地址报错

--- src/main.js
+++ src/main.js
@@ -6,6 +6,13 @@ import 'element-ui/lib/theme-chalk/index.css'
 // eslint-disable-next-line no-unused-vars
 import animate from 'animate.css'
 
+import VueRouter from "vue-router";
+// 解决两次访问相同路由地址报错
+const originalPush = VueRouter.prototype.push
+VueRouter.prototype.push = function push(location) {
+  return originalPush.call(this, location).catch(err => err)
+}
+
 import {Container, Header, Aside, Main, Footer,Row,Col,Message} from 'element-ui';
 Vue.use(Container);
 Vue.use(Header);
@@ -17,6 +24,8 @@ Vue.use(Col);
 
 Vue.prototype.$message = Message;
 Vue.config.productionTip = false;
+
+
 
 new Vue({
   store,

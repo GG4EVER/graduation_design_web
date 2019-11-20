3298101dcb15c6b7d1e6270fc3fc086e4335cdf1
荷包蛋 <43177271+GG4EVER@users.noreply.github.com>
2019-11-20 20:06:19 +0800

    增加了几个全局样式

--- src/App.vue
+++ src/App.vue
@@ -20,6 +20,8 @@
 </script>
 
 <style>
+    @import "assets/css/egg_paint.css";
+
     body{
         margin: 0;
         padding: 0;
@@ -48,5 +50,17 @@
     /*滑动块禁止时按钮的颜色(机器验证成功的颜色)*/
     .el-slider__runway.disabled .el-slider__button {
         border-color: #67C23A !important;
+    }
+
+    .el-menu-item-group__title{
+        padding: 0 !important;
+    }
+
+    .is-opened > .el-submenu__title{
+        color: #ffffff !important;
+    }
+
+    .is-opened > .el-submenu__title i{
+        color: #ffffff !important;
     }
 </style>

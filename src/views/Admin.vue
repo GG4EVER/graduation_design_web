0f2297dba8c1590c4818ea365d45c6fdeec16023
荷包蛋 <43177271+GG4EVER@users.noreply.github.com>
2019-11-20 23:04:02 +0800

    完善管理员页面

--- src/views/Admin.vue
+++ src/views/Admin.vue
@@ -1,13 +1,13 @@
 <template>
-    <el-container class="egg-full-screen">
+    <el-container class="egg-full-screen animated fadeIn">
         <el-aside class="egg-full-height" :class="isCollapse?'egg-aside-close':'egg-aside-open'">
             <admin-aside :is-collapse="isCollapse"></admin-aside>
         </el-aside>
         <el-container class="egg-no-padding">
             <el-header class="egg-no-padding">
-                <admin-header :is-collapse="isCollapse" @listenCollapseButton="changeCollapse"></admin-header>
+                <admin-header :is-collapse="isCollapse" @listenCollapseButton="changeCollapse" @listenLogOut="logout" @listenUpdateInfo="updateAdminInfo"></admin-header>
             </el-header>
-            <el-main class="egg-no-padding">
+            <el-main>
                 <router-view/>
             </el-main>
         </el-container>
@@ -26,12 +26,19 @@
         },
         data() {
             return {
-                isCollapse: true,//侧边栏的菜单是否关闭
+                isCollapse: false,//侧边栏的菜单是否关闭
             };
         },
         methods:{
             changeCollapse(){//切换菜单栏打开和关闭的状态
                 this.isCollapse = !this.isCollapse;
+            },
+            updateAdminInfo(){//修改管理员资料
+                this.$message("修改管理员资料");
+            },
+            logout(){//监听子组件的点击退出登录事件
+                this.$message("退出登录");
+                this.$router.push("/login")
             }
         }
     }
@@ -40,7 +47,7 @@
 <style scoped>
     .egg-aside-open{
         transition: 0.5s;
-        width: 300px !important;
+        width: 250px !important;
     }
 
     .egg-aside-close{

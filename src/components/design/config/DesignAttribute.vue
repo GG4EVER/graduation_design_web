1afae014e5b46516338fa0ae9b03190b5c23aadd
荷包蛋 <43177271+GG4EVER@users.noreply.github.com>
2020-02-13 01:04:38 +0800

    增加修改属性组件

--- src/components/design/config/DesignAttribute.vue
+++ src/components/design/config/DesignAttribute.vue
@@ -1,15 +1,47 @@
 <template>
     <!--    属性-->
     <el-col :span="24" class="design-attribute">
-        <el-col :span="20" :offset="2" class="design-no-checked-box egg-not-copy">
+        <el-col v-if="currComponentIndex != -1" :span="20" :offset="2">
+            <component :is="attributeComponentName" :component-attribute="currComponent.attribute" @listenSaveAttribute="saveAttribute"></component>
+        </el-col>
+        <el-col v-else :span="20" :offset="2" class="design-no-checked-box egg-not-copy">
             当前没有选中任何元素
         </el-col>
     </el-col>
 </template>
 
 <script>
+    import store from "../../../store"
+    import UniButtonAttribute from "./attribute/UniButtonAttribute";
+    import UniImageAttribute from "./attribute/UniImageAttribute";
     export default {
-        name: "DesignAttribute"
+        name: "DesignAttribute",
+        store,
+        components:{
+            UniButtonAttribute,
+            UniImageAttribute
+        },
+        watch:{
+            "$store.state.currComponentIndex":function(newVal){
+                this.currComponentIndex = newVal;
+                if(newVal != -1){//如果选中了组件，则获取对应的属性
+                    this.currComponent = store.state.currPageComponents[newVal];
+                    this.attributeComponentName = this.$AttributeComponentMap[this.currComponent.name];
+                }
+            }
+        },
+        data(){
+            return{
+                currComponentIndex:-1,//当前选中组件的索引
+                currComponent:{},//当前选中的组件对象
+                attributeComponentName:""
+            }
+        },
+        methods:{
+            saveAttribute(){
+                store.commit("saveComponentAttribute");
+            }
+        }
     }
 </script>
 

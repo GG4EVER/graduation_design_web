71a158321b186ff03644703c396a307404471035
荷包蛋 <43177271+GG4EVER@users.noreply.github.com>
2020-02-13 01:02:17 +0800

    增加修改图片属性组件

---
+++ src/components/design/config/attribute/UniImageAttribute.vue
@@ -0,0 +1,14 @@
+<template>
+    <!--    修改图片的属性-->
+    <div>修改图片的属性</div>
+</template>
+
+<script>
+    export default {
+        name: "UniImageAttribute"
+    }
+</script>
+
+<style scoped>
+
+</style>

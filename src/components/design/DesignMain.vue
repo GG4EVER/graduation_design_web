6af024761a95a9991cb808dd1a5c97797f5ec57e
荷包蛋 <43177271+GG4EVER@users.noreply.github.com>
2020-02-13 01:06:06 +0800

    修改部分bug

--- src/components/design/DesignMain.vue
+++ src/components/design/DesignMain.vue
@@ -1,43 +1,64 @@
 <template>
-    <el-row class="design-show-box">
-        <el-col :lg="11" :md="15" :sm="18" :xs="20">
-            <div class="design-phone">
-                <el-col :span="22" :offset="1" class="design-phone-header">
-                    <div class="design-phone-camera"></div>
-                    <div class="design-phone-receiver"></div>
-                </el-col>
-                <el-col :span="22">
-                    <div class="design-phone-screen-navigation-bar egg-not-copy" :style="'background-color: ' + navigationBarSetting.navigationBarBackgroundColor + ';color:' + navigationBarSetting.navigationBarTextStyle ">
-                        {{navigationBarSetting.navigationBarTitleText}}
-                    </div>
-                    <div class="design-phone-screen" :class="currPageComponents.length != 0 ? 'design-phone-screen-has' : ''" @click="clickDesignPhoneScreen" ref="phoneScreen">
-                        <template v-if="currPageComponents.length != 0">
-                            <div class="design-main-component" v-for="(component,index) in currPageComponents" :key="index" @click.stop="clickComponent(index)">
-                                <component :is="component.name" :component-attribute="component.attribute" :component-style="component.style"></component>
-                                <div class="design-main-component-mark"></div>
-                            </div>
-                        </template>
-                    </div>
-                </el-col>
-            </div>
-        </el-col>
-        <el-col class="hidden-sm-and-down design-main-util-box" :lg="3" :md="4">
-            <el-tooltip effect="dark"  content="删除" placement="right">
-                <div v-show="currComponentIndex != -1" class="design-main-util-item design-main-util-item-delete animated" :class="currComponentIndex != -1 ? 'fadeIn' : 'fadeOut'" @click="deleteComponent"><i class="el-icon-delete"></i></div>
-            </el-tooltip>
-            <el-tooltip effect="dark"  content="向上移动" placement="right">
-                <div v-show="currComponentIndex != -1" class="design-main-util-item design-main-util-item-up animated" :class="currComponentIndex != -1 ? 'fadeIn' : 'fadeOut'" @click="moveUpComponent"><i class="el-icon-sort-up"></i></div>
-            </el-tooltip>
-            <el-tooltip effect="dark"  content="向下移动" placement="right">
-                <div v-show="currComponentIndex != -1" class="design-main-util-item design-main-util-item-down animated" :class="currComponentIndex != -1 ? 'fadeIn' : 'fadeOut'" @click="moveDownComponent"><i class="el-icon-sort-down"></i></div>
-            </el-tooltip>
-            <el-tooltip effect="dark"  content="清空所有内容" placement="right">
-                <div v-show="currComponentIndex == -1" class="design-main-util-item design-main-util-item-top animated" :class="currComponentIndex == -1 ? 'fadeIn' : 'fadeOut'" @click="deleteAllComponent"><i class="el-icon-delete"></i></div>
-            </el-tooltip>
-            <el-tooltip effect="dark"  content="页面回到顶部" placement="right">
-                <div class="design-main-util-item design-main-util-item-top" @click="scrollBack"><i class="el-icon-caret-top"></i></div>
-            </el-tooltip>
-        </el-col>
+    <el-row>
+        <div class="design-show-box" @click="clickDesignShowBox">
+            <el-col :lg="11" :md="15" :sm="18" :xs="20">
+                <div class="design-phone">
+                    <el-col :span="22" :offset="1" class="design-phone-header">
+                        <div class="design-phone-camera"></div>
+                        <div class="design-phone-receiver"></div>
+                    </el-col>
+                    <el-col :span="22">
+                        <div class="design-phone-screen-navigation-bar egg-not-copy"
+                             :style="'background-color: ' + navigationBarSetting.navigationBarBackgroundColor + ';color:' + navigationBarSetting.navigationBarTextStyle ">
+                            {{navigationBarSetting.navigationBarTitleText}}
+                        </div>
+                        <div class="design-phone-screen"
+                             :class="currPageComponents.length != 0 ? 'design-phone-screen-has' : ''" ref="phoneScreen">
+                            <template v-if="currPageComponents.length != 0">
+                                <div class="design-main-component" v-for="(component,index) in currPageComponents"
+                                     :key="index" @click.stop="clickComponent(index)">
+                                    <component :is="component.name" :component-attribute="component.attribute"
+                                               :component-style="component.style"></component>
+                                    <div class="design-main-component-mark animated"
+                                         :class="index == currComponentIndex ? 'flash' : ''"
+                                         :style="index == currComponentIndex ? 'display:block;' : ''"></div>
+                                </div>
+                            </template>
+                        </div>
+                    </el-col>
+                </div>
+            </el-col>
+            <el-col class="hidden-sm-and-down design-main-util-box" :lg="3" :md="4">
+                <el-tooltip effect="dark" content="删除" placement="right">
+                    <div v-show="currComponentIndex != -1"
+                         class="design-main-util-item design-main-util-item-delete animated"
+                         :class="currComponentIndex != -1 ? 'fadeIn' : 'fadeOut'" @click.stop="deleteComponent"><i
+                            class="el-icon-delete"></i></div>
+                </el-tooltip>
+                <el-tooltip effect="dark" content="向上移动" placement="right">
+                    <div v-show="currComponentIndex != -1 && currComponentIndex != 0"
+                         class="design-main-util-item design-main-util-item-up animated"
+                         :class="currComponentIndex != -1 ? 'fadeIn' : 'fadeOut'" @click.stop="moveComponent"><i
+                            class="el-icon-sort-up"></i></div>
+                </el-tooltip>
+                <el-tooltip effect="dark" content="向下移动" placement="right">
+                    <div v-show="currComponentIndex != -1 && currComponentIndex != currPageComponents.length - 1"
+                         class="design-main-util-item design-main-util-item-down animated"
+                         :class="currComponentIndex != -1 ? 'fadeIn' : 'fadeOut'" @click.stop="moveComponent(false)"><i
+                            class="el-icon-sort-down"></i></div>
+                </el-tooltip>
+                <el-tooltip effect="dark" content="清空所有内容" placement="right">
+                    <div v-show="currComponentIndex == -1"
+                         class="design-main-util-item design-main-util-item-top animated"
+                         :class="currComponentIndex == -1 ? 'fadeIn' : 'fadeOut'" @click.stop="deleteAllComponent"><i
+                            class="el-icon-delete"></i></div>
+                </el-tooltip>
+                <el-tooltip effect="dark" content="页面回到顶部" placement="right">
+                    <div class="design-main-util-item design-main-util-item-top" @click.stop="scrollBack"><i
+                            class="el-icon-caret-top"></i></div>
+                </el-tooltip>
+            </el-col>
+        </div>
     </el-row>
 </template>
 
@@ -47,116 +68,137 @@
     import UniImage from "../../uni_components/components/UniImage";
     import {Tooltip} from "element-ui"
     import 'element-ui/lib/theme-chalk/display.css';
+
     export default {
         store,
         name: "DesignMain",
-        components:{
-            UniButton,UniImage,
-            [Tooltip.name]:Tooltip
+        components: {
+            UniButton, UniImage,
+            [Tooltip.name]: Tooltip
         },
-        data(){
-            return{
-                currPageComponents:[],//当前页面的组件
-                navigationBarSetting:{},//导航栏设置
-                currComponentIndex:-1
+        data() {
+            return {
+                currPageComponents: [],//当前页面的组件
+                navigationBarSetting: {},//导航栏设置
+                currComponentIndex: -1
             }
         },
-        watch:{
-            "$store.state.currPageComponents":function (newVal) {//监听当前页面组件列表
-                if(newVal){
+        watch: {
+            "$store.state.currPageComponents": function (newVal) {//监听当前页面组件列表
+                if (newVal) {
                     this.currPageComponents = newVal;
                     this.currComponentIndex = -1;
-                    this.$nextTick(()=>{
+                    this.$nextTick(() => {
                         this.$refs.phoneScreen.scrollTop = this.$refs.phoneScreen.scrollHeight;
                     });
                 }
             },
-            "$store.state.globalStyle":function(newVal){
-                if(newVal){
+            "$store.state.globalStyle": function (newVal) {
+                if (newVal) {
                     this.navigationBarSetting = newVal;
                 }
             }
         },
-        methods:{
+        methods: {
             /**
-             * 点击手机屏幕
+             * 点击组件以外的空白区域
              */
-            clickDesignPhoneScreen(){
-                this.currComponentIndex = -1;
+            clickDesignShowBox() {
+                if(this.currComponentIndex != -1){
+                    this.currComponentIndex = -1;//取消选择当前组件
+                    store.commit("setCurrComponentIndex",-1);
+                }else{
+                    window.console.log(store.state.currPageComponents)
+                }
             },
             /**
              * 点击组件
              * @param index 索引
              */
-            clickComponent(index){
-                this.currComponentIndex = index;
+            clickComponent(index) {
+                if (this.currComponentIndex != index) {
+                    this.currComponentIndex = index;
+                    store.commit("setCurrComponentIndex",index);
+                } else {
+                    this.currComponentIndex = -1;
+                    store.commit("setCurrComponentIndex",-1);
+                }
             },
             /**
              * 删除组件
              */
-            deleteComponent(){
+            deleteComponent() {
                 this.$confirm({
                     title: "提示",
                     message: "确认要删除这个组件吗",
                     showCancelButton: true,
-                }).then(()=>{
+                }).then(() => {
                     //确认清除
-                    store.commit("deleteCurrPageComponents",{
-                        index:this.currComponentIndex,
-                        deleteAll:false
+                    store.commit("deleteCurrPageComponents", {
+                        index: this.currComponentIndex,
+                        deleteAll: false
                     });
-                }).catch(()=>{
+                    this.clickComponent(-1);
+                }).catch(() => {
                 })
             },
             /**
              * 删除当前页面所有组件
              */
-            deleteAllComponent(){
+            deleteAllComponent() {
                 this.$confirm({
                     title: "提示",
                     message: "确认要清除所有内容吗",
                     showCancelButton: true,
-                }).then(()=>{
+                }).then(() => {
                     //确认清除
-                    store.commit("deleteCurrPageComponents",{
-                        index:-1,
-                        deleteAll:true
+                    store.commit("deleteCurrPageComponents", {
+                        index: -1,
+                        deleteAll: true
                     });
-                }).catch(()=>{
+                    this.clickComponent(-1);
+                }).catch(() => {
                 })
             },
             /**
-             * 向上移动组件
+             * 移动组件,默认向上
              */
-            moveUpComponent(){
-
-            },
-            /**
-             * 向下移动组件
-             */
-            moveDownComponent(){
-
+            moveComponent(isUp = true) {
+                store.commit("moveCurrPageComponents", {
+                    isUp: isUp,
+                    index: this.currComponentIndex
+                });
+                if (isUp) {
+                    this.currComponentIndex--;
+                } else {
+                    this.currComponentIndex++;
+                }
             },
             /**
              * 页面滚动至顶部
              */
-            scrollBack(){
-                let time = setInterval(()=>{
-                    if(this.$refs.phoneScreen.scrollTop >= 80){
+            scrollBack() {
+                let time = setInterval(() => {
+                    if (this.$refs.phoneScreen.scrollTop >= 80) {
                         this.$refs.phoneScreen.scrollTop -= 80;
-                    }else{
+                    } else {
                         this.$refs.phoneScreen.scrollTop = 0;
                         clearInterval(time);
                     }
-                },50);
+                }, 50);
             },
         },
         created() {
             this.navigationBarSetting = store.state.globalStyle;
-            // this.currPageComponents = store.state.pageComponents[store.state.pages[store.state.currPageIndex].name];
-            // if(!this.currPageComponents){//如果不存在,则新建
-            //     this.currPageComponents = new Array();
-            // }
+            if(store.state.pages.length !=0 ){//如果页面数组不为空
+                this.currPageComponents = store.state.pageComponents[store.state.pages[store.state.currPageIndex].name];
+                window.console.log(store.state.pageComponents)
+                window.console.log(store.state.currPageComponents)
+                window.console.log(this.currPageComponents)
+                if(!this.currPageComponents){
+                    this.currPageComponents = [];
+                }
+            }
         }
     }
 </script>
@@ -203,9 +245,9 @@
         background-color: #eeeeee;
     }
 
-    .design-phone-screen-navigation-bar{
+    .design-phone-screen-navigation-bar {
         height: 38px;
-        position:absolute;
+        position: absolute;
         top: 40px;
         width: 100%;
         background-color: #7B7B7B;
@@ -217,7 +259,7 @@
     }
 
     .design-phone-screen {
-        position:absolute;
+        position: absolute;
         top: 78px;
         bottom: 25px;
         right: 0;
@@ -227,59 +269,59 @@
         background-color: #fbfbfb;
     }
 
-    .design-phone-screen-has{
+    .design-phone-screen-has {
         background-color: #ffffff;
     }
 
-    .design-phone-screen::-webkit-scrollbar-track
-    {
-        -webkit-box-shadow: inset 0 0 5px rgba(255,255,255,0.3);
+    .design-phone-screen::-webkit-scrollbar-track {
+        -webkit-box-shadow: inset 0 0 5px rgba(255, 255, 255, 0.3);
         border-radius: 5px;
-        background-color: rgba(255,255,255,0.8);
+        background-color: rgba(255, 255, 255, 0.8);
     }
 
     /*定义滚动条高宽及背景 高宽分别对应横竖滚动条的尺寸*/
-    .design-phone-screen::-webkit-scrollbar
-    {
-        width: 0;/*0隐藏滚动条*/
-        background-color: rgba(255,255,255,0);
+    .design-phone-screen::-webkit-scrollbar {
+        width: 0; /*0隐藏滚动条*/
+        background-color: rgba(255, 255, 255, 0);
     }
 
     /*定义滑块 内阴影+圆角*/
-    .design-phone-screen::-webkit-scrollbar-thumb
-    {
+    .design-phone-screen::-webkit-scrollbar-thumb {
         border-radius: 5px;
-        -webkit-box-shadow: inset 0 0 5px rgba(255,255,255,0.3);
+        -webkit-box-shadow: inset 0 0 5px rgba(255, 255, 255, 0.3);
         background-color: #f0f0f0;
     }
 
-    .design-main-component{
+    .design-main-component {
         position: relative;
         cursor: pointer;
+        margin: 2px 0;
     }
 
-    .design-main-component:hover .design-main-component-mark{
-        display: block;
-    }
+    /*.design-main-component:hover .design-main-component-mark{*/
+    /*    display: block;*/
+    /*}*/
 
-    .design-main-component-mark{
+    .design-main-component-mark {
         display: none;
         position: absolute;
         top: 0;
         height: 100%;
         width: 100%;
         background-color: transparent;
-        box-shadow: 0 0 15px #cccccc inset;
+        box-shadow: 3px 3px 0px #8192d6 inset, -3px -3px 0px #8192d6 inset, 0px 0px 20px #8192d6;
+        animation-iteration-count: infinite;
+        animation-duration: 2.5s;
     }
 
-    .design-main-util-box{
+    .design-main-util-box {
         display: flex;
         justify-content: center;
         flex-direction: column;
         padding-left: 1.5rem;
     }
 
-    .design-main-util-item{
+    .design-main-util-item {
         height: 2.3rem;
         width: 2.3rem;
         margin: 3px 0;
@@ -293,46 +335,46 @@
         outline: none;
     }
 
-    .design-main-util-item-delete{
+    .design-main-util-item-delete {
         transition: 0.4s;
         border: 2px solid #e76ca3;
         color: #e76ca3;
         background-color: #fec2dc;
     }
 
-    .design-main-util-item-delete:hover{
+    .design-main-util-item-delete:hover {
         transition: 0.4s;
         color: #f0f0f0;
         background-color: #e76ca3;
     }
 
-    .design-main-util-item-up{
+    .design-main-util-item-up {
         transition: 0.4s;
         border: 2px solid #e2b911;
         color: #e2b911;
         background-color: #fff3b6;
     }
 
-    .design-main-util-item-up:hover{
+    .design-main-util-item-up:hover {
         transition: 0.4s;
         color: #f0f0f0;
         background-color: #e2b911;
     }
 
-    .design-main-util-item-down{
+    .design-main-util-item-down {
         transition: 0.4s;
         border: 2px solid #26a2d5;
         color: #26a2d5;
         background-color: #79d6ff;
     }
 
-    .design-main-util-item-down:hover{
+    .design-main-util-item-down:hover {
         transition: 0.4s;
         color: #f0f0f0;
         background-color: #26a2d5;
     }
 
-    .design-main-util-item-top{
+    .design-main-util-item-top {
         transition: 0.4s;
         margin: 20px 0;
         border: 2px solid #5B5B5B;
@@ -340,7 +382,7 @@
         background-color: #ffffff;
     }
 
-    .design-main-util-item-top:hover{
+    .design-main-util-item-top:hover {
         transition: 0.4s;
         color: #f0f0f0;
         background-color: #5B5B5B;

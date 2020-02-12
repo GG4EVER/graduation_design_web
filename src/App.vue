c9d47198f100c7869db4d20ff1ece600d145d983
荷包蛋 <43177271+GG4EVER@users.noreply.github.com>
2020-02-13 01:06:23 +0800

    增加全局样式，修复bug

--- src/App.vue
+++ src/App.vue
@@ -224,4 +224,15 @@
         border-bottom-left-radius: 10px;
         border-bottom-right-radius: 10px;
     }
+
+    .attribute-radio-group{
+        display: flex;
+        flex-wrap: wrap;
+        width: 100%;
+        justify-content: space-between;
+    }
+
+    .attribute-radio-group .el-radio{
+        margin: 5px 4px !important;
+    }
 </style>

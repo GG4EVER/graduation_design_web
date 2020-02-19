c83bcae7bdd1198653666e43c5db7742603a242e
荷包蛋 <43177271+GG4EVER@users.noreply.github.com>
2020-02-19 22:43:14 +0800

    新增用户项目管理

--- src/views/user/UserProject.vue
+++ src/views/user/UserProject.vue
@@ -1,10 +1,73 @@
 <template>
-    <div>用户项目页面</div>
+    <el-table
+            :data="tableData"
+            stripe
+            style="width: 100%">
+        <el-table-column
+                prop="id"
+                label="项目ID"
+                width="180">
+        </el-table-column>
+        <el-table-column
+                prop="name"
+                label="项目名称"
+                width="180">
+        </el-table-column>
+        <el-table-column
+                prop="description"
+                label="项目描述">
+        </el-table-column>
+        <el-table-column
+                prop="creator"
+                width="180"
+                label="创建者">
+        </el-table-column>
+        <el-table-column
+                prop="lastModified"
+                label="上次修改时间"
+                width="180">
+        </el-table-column>
+    </el-table>
 </template>
 
 <script>
+    import {Table, TableColumn} from "element-ui"
+
     export default {
-        name: "UserProject"
+        name: "UserProject",
+        components: {
+            [Table.name]: Table,
+            [TableColumn.name]: TableColumn
+        },
+        data() {
+            return {
+                tableData: [{
+                    id: "项目名",
+                    lastModified: '2016-05-02',
+                    name: '项目名',
+                    description: '项目描述',
+                    creator: "创建者"
+                }, {
+                    id: "项目名",
+                    lastModified: '2016-05-02',
+                    name: '项目名',
+                    description: '项目描述',
+                    creator: "创建者"
+                }, {
+                    id: "项目名",
+                    lastModified: '2016-05-02',
+                    name: '项目名',
+                    description: '项目描述',
+                    creator: "创建者"
+                }, {
+                    id: "项目名",
+                    lastModified: '2016-05-02',
+                    name: '项目名',
+                    description: '项目描述',
+                    creator: "创建者"
+                }]
+            }
+        }
     }
 </script>
 

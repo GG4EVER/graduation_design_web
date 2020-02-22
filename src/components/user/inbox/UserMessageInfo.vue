9d8745da861d4c304c8fcd96d95c758a69ea4ea2
荷包蛋 <43177271+GG4EVER@users.noreply.github.com>
2020-02-22 21:42:49 +0800

    完善用户站内消息页面

---
+++ src/components/user/inbox/UserMessageInfo.vue
@@ -0,0 +1,138 @@
+<template>
+    <el-dialog class="user-message-dialog" width="60%" :visible.sync="IsShow" :modal-append-to-body="false" top="10vh" :before-close="closeMessage">
+        <div class="user-project-info-title egg-not-copy" slot="title"><i class="el-icon-chat-dot-square el-icon--left user-project-info-icon"></i>{{MessageInfo.title}}</div>
+        <el-col :span="24">
+            <el-col :span="22" :offset="1">
+                <el-form>
+                    <el-form-item>
+                        <div slot="label">
+                            <div class="user-message-from-info egg-not-copy">
+                                <img src="../../../assets/images/boy.svg" />
+                                <span>{{MessageInfo.from}}</span>
+                            </div>
+                        </div>
+                        <div class="user-message-date egg-not-copy">
+                            {{MessageInfo.date}}
+                        </div>
+                    </el-form-item>
+                    <el-form-item>
+                        <div class="user-message-content-top-wall"><div class="user-message-triangle"></div></div>
+                        <div class="user-message-content">
+                            {{MessageInfo.content}}
+                        </div>
+                        <div class="user-message-content-bottom-wall"></div>
+                    </el-form-item>
+                </el-form>
+            </el-col>
+        </el-col>
+        <div slot="footer" class="user-project-info-button-box">
+            <el-button class="animated fadeIn" round type="danger" @click="closeMessage" icon="el-icon-close">关闭</el-button>
+        </div>
+    </el-dialog>
+</template>
+
+<script>
+    import {Dialog, Button, Form, FormItem, Input} from "element-ui"
+    export default {
+        name: "UserMessageInfo",
+        props:{
+            IsShow:{
+              type:Boolean
+            },
+            MessageInfo:{
+                type:Object
+            }
+        },
+        components: {
+            [Dialog.name]: Dialog,
+            [Button.name]: Button,
+            [Form.name]: Form,
+            [FormItem.name]: FormItem,
+            [Input.name]: Input
+        },
+        methods:{
+            closeMessage(){
+                this.$emit("listenClose")
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    .user-message-from-info{
+        height: 50px;
+        display: flex;
+        justify-content: space-around;;
+        align-items: center;
+    }
+
+    .user-message-from-info > img{
+        height: 36px;
+        width: 36px;
+        margin-right: 8px;
+    }
+
+    .user-message-from-info > span{
+        font-size: 18px;
+        color: #1f2d3d;
+    }
+
+    .user-message-date{
+        height: 50px;
+        text-align: right;
+        line-height: 40px;
+    }
+
+    .user-message-content-top-wall{
+        height: 16px;
+        background-color: #efefef;
+        border-top-left-radius: 14px;
+        border-top-right-radius: 14px ;
+        position: relative;
+    }
+
+    .user-message-content-bottom-wall{
+        height: 16px;
+        background-color: #efefef;
+        border-bottom-left-radius: 14px;
+        border-bottom-right-radius: 14px;
+        position: relative;
+        margin-bottom: 20px;
+    }
+
+    .user-message-content{
+        background-color: #efefef;
+        padding: 0px 20px;
+        height: 250px;
+        font-size: 15px;
+        color: #1f2d3d;
+        position: relative;
+        overflow-y: scroll;
+    }
+
+    .user-message-triangle {
+        position: absolute;
+        width: 0;
+        height: 0;
+        border: 14px solid;
+        border-color: transparent  transparent #efefef transparent;
+        top: -27px;
+        left: 38px;
+    }
+
+    .user-project-info-title {
+        text-align: center;
+        font-size: 20px;
+        font-weight: bold;
+        color: #333333;
+    }
+
+    .user-project-info-button-box {
+        text-align: center;
+    }
+
+    .user-project-info-icon{
+        color: #61c4fd;
+    }
+
+</style>
--- src/views/user/InBox.vue
+++ src/views/user/InBox.vue
@@ -1,13 +1,143 @@
 <template>
-    <div>用户收件箱</div>
+    <el-row>
+        <el-col class="inbox-button-box" :span="24">
+            <el-button v-if="messages.length != 0" type="danger" @click="deleteMessage" size="small"
+                       icon="el-icon-delete">删除
+            </el-button>
+        </el-col>
+        <el-col :span="24">
+            <el-table
+                    class="user-project-table"
+                    :data="messages"
+                    style="width: 100%" row-class-name="egg-cursor egg-table-row" @row-click="lookMessage"
+                    @selection-change="selectMessage" empty-text="当前暂无消息">
+                <el-table-column
+                        type="selection"
+                        width="55">
+                </el-table-column>
+                <el-table-column
+                        label="发件人"
+                        width="150">
+                    <template slot-scope="scope">
+                        <span class="inbox-message-form"> {{scope.row.from}}</span>
+                    </template>
+                </el-table-column>
+                <el-table-column
+                        label="主题"
+                        width="160">
+                    <template slot-scope="scope">
+                        <span class="inbox-message-title"> {{scope.row.title}}</span>
+                    </template>
+                </el-table-column>
+                <el-table-column
+                        label="内容">
+                    <template slot-scope="scope">
+                        <span class="inbox-message-content"> {{scope.row.content}}</span>
+                    </template>
+                </el-table-column>
+                <el-table-column
+                        label="时间"
+                        width="180">
+                    <template slot-scope="scope">
+                        <span class="inbox-message-date"> {{scope.row.date}}</span>
+                    </template>
+                </el-table-column>
+            </el-table>
+        </el-col>
+        <user-message-info :is-show="showMessage" :message-info="currMessage"
+                           @listenClose="closeMessage"></user-message-info>
+    </el-row>
 </template>
 
 <script>
+    import {Table, TableColumn, Button} from "element-ui"
+    import UserMessageInfo from "../../components/user/inbox/UserMessageInfo";
+
     export default {
-        name: "InBox"
+        name: "InBox",
+        components: {
+            UserMessageInfo,
+            [Table.name]: Table,
+            [TableColumn.name]: TableColumn,
+            [Button.name]: Button
+        },
+        data() {
+            return {
+                messages: [{
+                    from: "管理员",
+                    title: "实名认证审核进度",
+                    content: "恭喜你，已通过实名认证1",
+                    date: "2020-02-19",
+                    isRead: true,
+                }, {
+                    from: "管理员",
+                    title: "实名认证审核进度",
+                    content: "恭喜你，已通过实名认证2",
+                    date: "2020-02-20",
+                    isRead: false,
+                }],
+                showMessage: false,
+                currMessage: {},
+                selectedMessages: [],//已经选择的消息列表
+            }
+        },
+        methods: {
+            lookMessage(row) {
+                this.showMessage = true;
+                this.currMessage = row;
+            },
+            closeMessage() {
+                this.showMessage = false;
+                this.currMessage = {};
+            },
+            selectMessage(selection) {
+                this.selectedMessages = selection;
+            },
+            deleteMessage() {
+                //如果有选择
+                if (this.selectedMessages.length != 0) {
+                    this.$confirm({
+                        title: "是否要删除消息",
+                        showCancelButton: true
+                    }).then(() => {
+                        let originMessages = JSON.stringify(this.messages);
+                        for (let i = 0; i < this.selectedMessages.length; i++) {
+                            let deleteItem = JSON.stringify(this.selectedMessages[i]);
+                            if (originMessages.indexOf(deleteItem + ",") != -1) {
+                                originMessages = originMessages.replace(deleteItem + ",", "");
+                                continue;
+                            }
+                            originMessages = originMessages.replace(deleteItem, "");
+                        }
+                        this.messages = JSON.parse(originMessages)
+                    }).catch(() => {
+                    })
+                } else {
+                    this.$message.info("请先勾选要删除的消息")
+                }
+            },
+        }
     }
 </script>
 
 <style scoped>
+    .inbox-button-box {
+        padding: 10px 0;
+    }
 
+    .inbox-message-form {
+        font-weight: bold;
+    }
+
+    .inbox-message-title {
+        color: #666666;
+    }
+
+    .inbox-message-content {
+        color: #5B5B5B;
+    }
+
+    .inbox-message-date {
+        color: #5B5B5B;
+    }
 </style>

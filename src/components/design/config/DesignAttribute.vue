96269924dd19a6e1dc4aecd801a3c86dc9e844fe
荷包蛋 <43177271+GG4EVER@users.noreply.github.com>
2020-03-07 00:32:32 +0800

    Update DesignAttribute.vue

--- src/components/design/config/DesignAttribute.vue
+++ src/components/design/config/DesignAttribute.vue
@@ -1,9 +1,26 @@
 <template>
     <!--    属性-->
     <el-col :span="24" class="design-attribute">
-        <el-col v-if="currComponentIndex != -1" :span="22" :offset="2">
-            <component :is="attributeComponentName" :component-attribute="currComponent.attribute" @listenSaveAttribute="saveAttribute"></component>
-            <base-attribute :component-attribute="currComponent.style" @listenChangeComponentStyle="changeComponentStyle"></base-attribute>
+        <el-col v-if="currComponentIndex != -1" :span="23" :offset="1">
+            <el-tabs class="design-attribute-tabs" v-model="showTabIndex">
+                <el-tab-pane label="组件属性" name="0">
+                    <el-col :span="22" :offset="2">
+                        <component :is="attributeComponentName" :component-attribute="currComponent.attribute"
+                                   @listenSaveAttribute="saveAttribute"></component>
+                    </el-col>
+                </el-tab-pane>
+                <el-tab-pane  label="基础属性" name="1">
+                    <el-col :span="22" :offset="2">
+                        <base-attribute :component-attribute="currComponent.style"
+                                        @listenChangeComponentStyle="changeComponentStyle"></base-attribute>
+                    </el-col>
+                </el-tab-pane>
+                <el-tab-pane label="高级属性" name="2">
+                    <el-col :span="22" :offset="2">
+                        高级属性
+                    </el-col>
+                </el-tab-pane>
+            </el-tabs>
         </el-col>
         <el-col v-else :span="20" :offset="2" class="design-no-checked-box egg-not-copy">
             当前没有选中任何元素
@@ -16,42 +33,49 @@
     import BaseAttribute from "./attribute/BaseAttribute";
     import UniButtonAttribute from "./attribute/UniButtonAttribute";
     import UniImageAttribute from "./attribute/UniImageAttribute";
+    import UniSwiperAttribute from "./attribute/UniSwiperAttribute";
+    import {Tabs, TabPane} from "element-ui"
 
     export default {
         name: "DesignAttribute",
         store,
-        components:{
+        components: {
             BaseAttribute,
             UniButtonAttribute,
-            UniImageAttribute
+            UniImageAttribute,
+            UniSwiperAttribute,
+            [Tabs.name]: Tabs,
+            [TabPane.name]: TabPane
         },
-        watch:{
-            "$store.state.currComponentIndex":function(newVal){
+        watch: {
+            "$store.state.currComponentIndex": function (newVal) {
                 this.currComponentIndex = newVal;
-                if(newVal != -1){//如果选中了组件，则获取对应的属性
+                if (newVal != -1) {//如果选中了组件，则获取对应的属性
                     this.currComponent = store.state.currPageComponents[newVal];
                     this.attributeComponentName = this.$AttributeComponentMap[this.currComponent.name];
                 }
+                this.showTabIndex = "0";
             }
         },
-        data(){
-            return{
-                currComponentIndex:-1,//当前选中组件的索引
-                currComponent:{},//当前选中的组件对象
-                attributeComponentName:""
+        data() {
+            return {
+                showTabIndex: "0",
+                currComponentIndex: -1,//当前选中组件的索引
+                currComponent: {},//当前选中的组件对象
+                attributeComponentName: ""
             }
         },
-        methods:{
-            saveAttribute(){
+        methods: {
+            saveAttribute() {
                 store.commit("saveComponentAttribute");
             },
             /**
              * 监听修改组件样式
              * @param style
              */
-            changeComponentStyle(style){
+            changeComponentStyle(style) {
                 this.currComponent.style = style;
-                this.$set(store.state.currPageComponents,store.state.currComponentIndex,this.currComponent);
+                this.$set(store.state.currPageComponents, store.state.currComponentIndex, this.currComponent);
                 this.saveAttribute();
             }
         }

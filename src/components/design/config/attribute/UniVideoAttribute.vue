73e5d05f081e6b8b71e396a4fbecf155a9a41214
荷包蛋 <43177271+GG4EVER@users.noreply.github.com>
2020-04-15 00:12:37 +0800

    增加视频组件

---
+++ src/components/design/config/attribute/UniVideoAttribute.vue
@@ -0,0 +1,64 @@
+<template>
+    <el-form :model="ComponentAttribute" status-icon>
+        <el-col :span="24" class="design-setting-title egg-not-copy"><i class="el-icon-minus el-icon--left"></i>视频属性<i
+                class="el-icon-minus el-icon--right"></i></el-col>
+        <el-form-item>
+            <div slot="label" class="design-setting-label egg-not-copy">视频地址</div>
+            <el-input type="textarea" clearable resize="none" :autosize="{minRows: 2, maxRows:5}"
+                      v-model="ComponentAttribute.src" @change="changeAttribute"></el-input>
+        </el-form-item>
+        <el-form-item>
+            <div slot="label" class="design-setting-label egg-not-copy">是否自动播放</div>
+            <el-switch v-model="ComponentAttribute.selectable" @change="changeAttribute"></el-switch>
+        </el-form-item>
+        <el-form-item>
+            <div slot="label" class="design-setting-label egg-not-copy">是否循环播放</div>
+            <el-switch v-model="ComponentAttribute.loop" @change="changeAttribute"></el-switch>
+        </el-form-item>
+        <el-form-item>
+            <div slot="label" class="design-setting-label egg-not-copy">是否显示播放控件</div>
+            <el-switch v-model="ComponentAttribute.controls" @change="changeAttribute"></el-switch>
+        </el-form-item>
+        <el-form-item v-if="ComponentAttribute.controls">
+            <div slot="label" class="design-setting-label egg-not-copy">视频封面图链接</div>
+            <el-input type="textarea" clearable resize="none" :autosize="{minRows: 2, maxRows:5}"
+                      v-model="ComponentAttribute.poster" placeholder="如果不填则不显示封面"  @change="changeAttribute"></el-input>
+        </el-form-item>
+        <el-form-item>
+            <div slot="label" class="design-setting-label egg-not-copy">是否显示全屏按钮</div>
+            <el-switch v-model="ComponentAttribute['show-fullscreen-btn']" @change="changeAttribute"></el-switch>
+        </el-form-item>
+        <el-form-item>
+            <div slot="label" class="design-setting-label egg-not-copy">是否在底部显示播放按钮</div>
+            <el-switch v-model="ComponentAttribute['show-play-btn']" @change="changeAttribute"></el-switch>
+        </el-form-item>
+    </el-form>
+</template>
+
+<script>
+    import {Form, FormItem, Input, Switch} from "element-ui"
+
+    export default {
+        name: "UniVideoAttribute",
+        props: {
+            ComponentAttribute: {
+                type: Object
+            },
+        },
+        components: {
+            [Form.name]: Form,
+            [FormItem.name]: FormItem,
+            [Input.name]: Input,
+            [Switch.name]: Switch
+        },
+        methods: {
+            changeAttribute() {
+                this.$emit("listenSaveAttribute");
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/uni_components/components/UniVideo.vue
@@ -0,0 +1,65 @@
+<template>
+    <div class="uni-image-box" :class="ComponentAnimation" :style="ComponentStyle">
+        <div class="uni-image-no-image-icon"><i class="el-icon-picture"></i></div>
+        <div class="uni-image-no-image-info">视频组件实际效果需要在预览功能中查看</div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "UniVideo",
+        props: {
+            ComponentAttribute: {//组件属性
+                type: Object,
+                default() {
+                    return {
+                        src: "",//视频资源地址
+                        autoplay: false,//是否自动播放
+                        loop: false,//是否循环播放
+                        controls: true,//是否显示播放控件
+                        "show-fullscreen-btn": true,//是否显示全屏按钮
+                        "show-play-btn": true,//是否显示视频底部控制栏的播放按钮
+                        poster: "",//视频封面地址，controls为true时才能设置
+                    }
+                }
+            },
+            ComponentStyle: {//额外的样式
+                type: String,
+                default: "",
+            },
+            ComponentAnimation: {//动画效果
+                type: String,
+                default: "",
+            },
+        },
+    }
+</script>
+
+<style scoped>
+    .uni-image-box {
+        position: relative;
+        width: 100%;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        flex-direction: column;
+        height: 12rem;
+        background-color: #f0f0f0;
+    }
+
+    .uni-image-no-image-icon {
+        padding: 0 15px;
+        font-size: 3.5rem;
+        text-align: center;
+        color: #7B7B7B;
+    }
+
+    .uni-image-no-image-info {
+        padding: 0 15px;
+        height: 3.5rem;
+        font-size: 13px;
+        font-weight: bold;
+        text-align: center;
+        color: #7B7B7B;
+    }
+</style>

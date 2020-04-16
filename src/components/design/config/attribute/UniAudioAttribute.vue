c91717acc62537e4648ccc88f4a2dddb6aa80002
荷包蛋 <43177271+GG4EVER@users.noreply.github.com>
2020-04-17 01:17:54 +0800

    增加音频组件

---
+++ src/components/design/config/attribute/UniAudioAttribute.vue
@@ -0,0 +1,47 @@
+<template>
+    <el-form :model="ComponentAttribute" status-icon>
+        <el-col :span="24" class="design-setting-title egg-not-copy"><i class="el-icon-minus el-icon--left"></i>音频属性<i
+                class="el-icon-minus el-icon--right"></i></el-col>
+        <el-form-item>
+            <div slot="label" class="design-setting-label egg-not-copy">音频地址</div>
+            <el-input type="textarea" clearable resize="none" :autosize="{minRows: 2, maxRows:5}"
+                      v-model="ComponentAttribute.src" @change="changeAttribute"></el-input>
+        </el-form-item>
+        <el-form-item>
+            <div slot="label" class="design-setting-label egg-not-copy">是否循环播放</div>
+            <el-switch v-model="ComponentAttribute.loop" @change="changeAttribute"></el-switch>
+        </el-form-item>
+        <el-form-item v-if="ComponentAttribute.controls">
+            <div slot="label" class="design-setting-label egg-not-copy">音频封面图链接</div>
+            <el-input type="textarea" clearable resize="none" :autosize="{minRows: 2, maxRows:5}"
+                      v-model="ComponentAttribute.poster" placeholder="如果不填则不显示封面"  @change="changeAttribute"></el-input>
+        </el-form-item>
+    </el-form>
+</template>
+
+<script>
+    import {Form, FormItem, Input, Switch} from "element-ui"
+    export default {
+        name: "UniAudioAttribute",
+        components: {
+            [Form.name]: Form,
+            [FormItem.name]: FormItem,
+            [Input.name]: Input,
+            [Switch.name]: Switch
+        },
+        props:{
+            ComponentAttribute: {//组件属性
+                type: Object
+            },
+        },
+        methods: {
+            changeAttribute() {
+                this.$emit("listenSaveAttribute");
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/uni_components/components/UniAudio.vue
@@ -0,0 +1,68 @@
+<template>
+    <!--    音频-->
+    <div :style="ComponentStyle" :class="ComponentAnimation">
+        <a-player :repeat="ComponentAttribute.loop ? 'repeat-one' : 'no-repeat'"  :music="audioInfo"></a-player>
+    </div>
+</template>
+
+<script>
+    import AudioPlayer  from 'vue-aplayer'
+    export default {
+        name: "UniAudio",
+        components: {
+            "a-player":AudioPlayer
+        },
+        props: {
+            ComponentAttribute: {//组件属性
+                type: Object,
+                default() {
+                    return null;
+                }
+            },
+            ComponentStyle: {//额外的样式
+                type: String,
+                default: "",
+            },
+            ComponentAnimation: {//动画效果
+                type: String,
+                default: "",
+            },
+        },
+        watch:{
+            ComponentAttribute(newVal){
+                if(newVal){
+                    this.audioInfo = {
+                        title: newVal.ComponentAttribute.name,
+                        artist: newVal.ComponentAttribute.author,
+                        src: newVal.ComponentAttribute.src,
+                        pic:newVal.ComponentAttribute.poster
+                    }
+                }
+            }
+        },
+        created() {
+            if(this.ComponentAttribute){
+                this.audioInfo = {
+                    title: this.ComponentAttribute.name,
+                    artist: this.ComponentAttribute.author,
+                    src: this.ComponentAttribute.src,
+                    pic:this.ComponentAttribute.poster
+                }
+            }
+        },
+        data() {
+            return {
+                audioInfo: {
+                    title: '',
+                    artist: '',
+                    src: '',
+                    pic:''
+                },
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>

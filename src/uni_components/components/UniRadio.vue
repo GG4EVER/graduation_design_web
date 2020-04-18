50dfbbf855954ea12fd3d57c0a716735276dc034
荷包蛋 <43177271+GG4EVER@users.noreply.github.com>
2020-04-19 02:12:32 +0800

    增加单选框组件

---
+++ src/uni_components/components/UniRadio.vue
@@ -0,0 +1,46 @@
+<template>
+    <div>
+        <div slot="label">{{ComponentAttribute.label}}</div>
+        <el-radio v-for="(option,index) in ComponentAttribute.options" :key="index" :label="option" v-model="radioTemp">{{option}}</el-radio>
+    </div>
+</template>
+
+<script>
+    import {Radio} from "element-ui";
+
+    export default {
+        name: "UniRadio",
+        components: {
+            [Radio.name]: Radio,
+        },
+        props: {
+            ComponentAttribute: {//组件属性
+                type: Object,
+                default() {
+                    return {
+                        name: "",//字段名
+                        label: "",//标题
+                        options: "",//选项
+                    }
+                }
+            },
+            ComponentStyle: {//额外的样式
+                type: String,
+                default: "",
+            },
+            ComponentAnimation: {//动画效果
+                type: String,
+                default: "",
+            },
+        },
+        data(){
+            return{
+                radioTemp:"",
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>

ba670c7e90122f732443d6065ff5462af9cfcc92
荷包蛋 <43177271+GG4EVER@users.noreply.github.com>
2020-05-10 15:14:09 +0800

    不影响的修改

--- src/components/admin/user/UserListComponent.vue
+++ src/components/admin/user/UserListComponent.vue
@@ -39,7 +39,7 @@
                 </el-table-column>
                 <el-table-column label="操作" width="100">
                     <template slot-scope="scope">
-                        <el-button type="primary" size="small" @click="lookUserInfo(scope.row)">查看资料</el-button>
+                        <el-button type="primary" size="small" @click.stop="lookUserInfo(scope.row)">查看资料</el-button>
                     </template>
                 </el-table-column>
                 <el-table-column width="100">
